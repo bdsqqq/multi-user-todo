7da3e91caf860e97436895f061d872024c40c700
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-10 16:50:23 -0300

    test: described navigation

--- cypress/integration/user.spec.ts
+++ cypress/integration/user.spec.ts
@@ -14,6 +14,20 @@ describe("Todo fetching", () => {
     cy.get("label").contains("delectus aut autem");
   });
 });
+
+describe("Navigation", () => {
+  it("should navigate to the home page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/user/1");
+
+    // Find a link with an href attribute containing "/" and click it
+    cy.get("a").contains("Back home").click();
+
+    // The new url should include "localhost:3000"
+    cy.url().should("contain", "localhost:3000");
+
+    // The new page should contain an h1 with "Click on a user to see their todo items"
+    cy.get("h1").contains("Click on a user to see their todo items");
   });
 });
 
