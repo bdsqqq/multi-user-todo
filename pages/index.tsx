6afc7bce2b6c884addd9102eb75faf2da06419ba
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-10 12:12:46 -0300

    feat: render fetched users on page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,35 @@
 import { useEffect } from "react";
 import useSWR from "swr";
 import { fetcher } from "@/lib/fetcher";
+import Head from "next/head";
 
 export default function Home() {
-  const { data, error } = useSWR(
+  const { data: users, error } = useSWR(
     "https://jsonplaceholder.typicode.com/users",
     fetcher
   );
 
   useEffect(() => {
-    if (data) console.log({ data });
+    if (users) console.log({ users });
     if (error) console.log({ error });
-  }, [data, error]);
+  }, [users, error]);
 
-  return <div />;
+  return (
+    <>
+      <Head>
+        <title>Multi User Todo</title>
+      </Head>
+      <div>
+        <header>
+          <nav></nav>
+        </header>
+        <main>
+          <ul>
+            {users.length > 0 &&
+              users.map((user, i) => <li key={user.id}>{user.name}</li>)}
+          </ul>
+        </main>
+      </div>
+    </>
+  );
 }
