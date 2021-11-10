1b4acbe926ad89997c15d99dc569f812cc224a67
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-10 12:01:37 -0300

    feat: fetch userData from API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+import { fetcher } from "@/lib/fetcher";
+
 export default function Home() {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) console.log({ data });
+    if (error) console.log({ error });
+  }, [data, error]);
+
   return <div />;
 }
