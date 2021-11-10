39823e5811593c0cd3c41537be75134df233dd67
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-10 15:38:54 -0300

    feat: setup theme with radix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const { mauve, crimson } = require("@radix-ui/colors");
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ...mauve,
+      ...crimson,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
